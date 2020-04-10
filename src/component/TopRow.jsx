882c1cb7120b96855f60a34453f371a11a9fe199
John Rossi <DACOOK1983@gamil.com>
2020-04-09 20:25:38 -0400

    as done as it gets

--- src/component/TopRow.jsx
+++ src/component/TopRow.jsx
@@ -3,11 +3,11 @@ import React, { useState } from "react";
 
 
 let TopRow = props => {
-  const { team1, team2, home, away } = useState( props);
+  const { team1, team2 } = useState( props);
   return (
     <div className="topRow">
       <div className="home">
-        <h2 className="home__name">{home}</h2>
+        <h2 className="home__name">Giants</h2>
 
         {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -15,7 +15,7 @@ let TopRow = props => {
       </div>
       <div className="timer">00:25</div>
       <div className="away">
-        <h2 className="away__name">{away}</h2>
+        <h2 className="away__name">Lions</h2>
         <div className="away__score">{team2}</div>
       </div>
     </div>
